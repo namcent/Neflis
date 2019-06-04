f7213116695ce6de741d9d2f27cd339a12ad1a18
Yaz <yaz_01hotamil.com>
2019-06-03 22:54:51 -0300

    jaja

--- src/Usuario.java
+++ src/Usuario.java
@@ -32,12 +32,3 @@ public class Usuario {
 
     }
 
-
-
-
-=======
-    public Boolean contQueVio(Contenido contenido){
-        return contQueVio.Stream().map(Contenido->contenido.getGenero()).
-                distint().collect( Collectors.toSet());}
-}
->>>>>>> master
